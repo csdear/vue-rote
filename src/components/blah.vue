e7e02c251c7775d869ddc2ddbab11bae02959b43
csdear <csdear138@gmail.com>
2019-09-12 07:06:52 -0500

    playing with importing js

---
+++ src/components/blah.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>{{ greeting }} World!</p>
+</template>
+
+ 
+
+<script>
+//bringing in external scripts
+import heyScript from "@/components/hey.js";
+
+module.exports = {
+  data: function () {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,26 @@
   </div>
 </template>
 
+
 <script>
+//Bringing in a external component
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
+//bringing in a external javascript file
+import heyScript from "@/components/hey.js";
+
+//could also do it like this
+  //var myScript = require('@/components/hey.js');
+
 export default {
+  //inline JS with data()
+  data() {
+    return {
+      helloTwo: console.log('INLINE HEY'),
+      //helloThree: myScript
+    }
+  },
   name: "home",
   components: {
     HelloWorld
