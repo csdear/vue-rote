95deefbbb427d172946cbb920e7f6dfc974f59ff
csdear <csdear138@gmail.com>
2019-09-23 06:55:16 -0500

    9 ways to do style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,18 @@
         >vue-cli documentation</a
       >.
     </p>
+    <p style="text-decoration: underline">Test inline style</p>
+    <p>Pellentesque suscipit maximus purus, in aliquet nibh mollis vel. <span style="text-decoration: underline">Nulla dictum posuere varius.</span> Nullam condimentum <br> risus sit amet elit commodo congue. Duis sit amet diam orci. Integer nec aliquam libero.</P> 
+    <!-- Xor. Styling Vue components -->
+    <p :style="{'color': textColor}">v-bind style example to data compt object</p>
+    <p :style="mystyle">v-bind style example to a computed property</p>
+    <p class="imRed">By class identifier</p>
+    <p :class="{underline: isUnderlined}">Dynamic based on data() if prop is true, will apply css class.</p>
+    <p :class="ogreClass"> Directly bind a string, and that will reference a data property</p>
+    <p :class="[ogreClass, ogreBold]"> Reference multiple CSS classes</p>
+    <p :class="{underline: isUnderlined, bold: isBold}"> Reference multiple CSS classes using an object inlined in the class binding</p>
+    <p :class="[textDecoration, {bold: isBold}]"> Hybrd, object inlined and an Array</p>
+    <p :class="paragraphClasses">Computed, multiple</p><!--xor9-->
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
@@ -100,14 +112,62 @@
 <script>
 export default {
   name: "HelloWorld",
+  data() {
+    return {
+      textColor: 'blue',
+      textDecoration: 'underline',
+      textWeight: 'bold',
+      isUnderlined: true,
+      isBold: true,
+      ogreClass: 'ogreClass',
+      ogreBold: 'ogreBold',
+    }
+  },
   props: {
     msg: String
+  },
+  computed: {
+    mystyle: function() {
+      return {
+        textDecoration: this.textDecoration,
+        textWeight: this.textWeight, 
+      }
+    },
+    paragraphClasses: function() {
+      return {
+        underlined: this.isUnderlined,
+        bold: this.isBold,
+        ogreClass: this.ogreClass,
+    }
   }
-};
+  }
+  };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+
+.ogreClass { 
+  color: green; 
+}
+
+.ogreBold { 
+  font-weight: bold; 
+}
+
+.underline { 
+  text-decoration: underline; 
+}
+
+.bold { 
+  font-weight: bold; 
+}
+
+.imRed { 
+  color: red; 
+}
+
 h3 {
   margin: 40px 0 0;
 }
