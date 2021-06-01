ba870ef059232631c1191f04f8f6ed25f591900d
Stuart Dear <stuart.dear@intermarkgroup.com>
2021-06-01 05:53:03 -0500

    jest tests, to confirm rendered content.

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,7 @@
+import Vue from 'vue';
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import BasicTableTest from "@/components/testing/BasicTableTest.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
@@ -8,5 +10,35 @@ describe("HelloWorld.vue", () => {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("HelloWorld.vue", () => {
+  it("should run this dummy text", () => {
+    expect('Dummy' + ' Test!').toEqual('Dummy Test!');
+  });
+});
+
+describe('BasicTableTest.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(BasicTableTest);
+    const vm = new Constructor().$mount();
+
+    expect(
+      vm.$el.querySelector('.ui.selectable thead tr th').textContent
+    ).toContain('Items');
+    expect(
+      vm.$el.querySelector('.ui.button').textContent
+    ).toContain('Add');
+    expect(
+      vm.$el.querySelector('.ui.label').textContent
+    ).toContain('Remove all');
+  });
+
+  it('should set correct default data', () => {
+    const initialData = BasicTableTest.data();
+
+    expect(initialData.item).toEqual('');
+    expect(initialData.items).toEqual([]);
   });
 });
